05a5af735d124a589a0fc7bce9a98123076f4e9b
Farzan Saketi <10725870+farzan6118@users.noreply.github.com>
2026-09-02 15:54:33 +0330

    ref: many changes in company

--- src/main/java/com/github/farzan6118/petclinic/config/JpaRepositoryConfig.java
+++ src/main/java/com/github/farzan6118/petclinic/config/JpaRepositoryConfig.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories(
-        basePackages = "com.github.farzan6118.petclinic.repository.jpa"
-)
+@EnableJpaRepositories(basePackages = "com.github.farzan6118.petclinic.repository")
 public class JpaRepositoryConfig {
 }
--- src/main/java/com/github/farzan6118/petclinic/config/RedisRepositoryConfig.java
+++ src/main/java/com/github/farzan6118/petclinic/config/RedisRepositoryConfig.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
-@EnableRedisRepositories(
-        basePackages = "com.github.farzan6118.petclinic.repository.redis"
-)
+@EnableRedisRepositories(basePackages = "com.github.farzan6118.petclinic.repository.redis")
 public class RedisRepositoryConfig {
 }
