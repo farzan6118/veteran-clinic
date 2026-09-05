d33d51c59945185ff7b78a6bd4198768f3810786
Farzan Saketi <10725870+farzan6118@users.noreply.github.com>
2026-09-05 12:31:33 +0330

    ref: some changes

--- src/main/java/com/github/farzan6118/petclinic/service/impl/VisitNotificationServiceImpl.java
+++ src/main/java/com/github/farzan6118/petclinic/service/impl/VisitNotificationServiceImpl.java
@@ -25,6 +25,8 @@ public class VisitNotificationServiceImpl implements VisitNotificationService {
     public void notifyVisitRescheduled(Visit visit, Pet pet, Vet vet, LocalDateTime oldVisitDate, String reason) {
         Owner owner = pet.getOwner();
 
+
+
         try {
             emailService.sendVetVisitRescheduledEmail(
                     vet.getEmail(),
