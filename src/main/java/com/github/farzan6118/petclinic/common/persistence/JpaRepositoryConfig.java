2a8631529571a985359e7c86c08147abe79731ae
Farzan Saketi <10725870+farzan6118@users.noreply.github.com>
2026-09-14 17:28:34 +0330

    feat: feature based folder structure

--- src/main/java/com/github/farzan6118/petclinic/common/persistence/JpaRepositoryConfig.java
+++
@@ -1,9 +0,0 @@
-package com.github.farzan6118.petclinic.common.persistence;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@Configuration
-@EnableJpaRepositories(basePackages = "com.github.farzan6118.petclinic.repository")
-public class JpaRepositoryConfig {
-}
--- src/main/java/com/github/farzan6118/petclinic/visit/repository/VisitRepository.java
+++ src/main/java/com/github/farzan6118/petclinic/visit/repository/VisitRepository.java
@@ -36,8 +36,8 @@ public interface VisitRepository extends JpaRepository<Visit, Long> {
             select case when count(v) > 0 then true else false end
             from Visit v
             where v.pet.uuid = :petUuid
-            and v.status not in (com.github.farzan6118.petclinic.model.constant.VisitStatus.CANCELLED,
-                                 com.github.farzan6118.petclinic.model.constant.VisitStatus.COMPLETED)
+            and v.status not in (com.github.farzan6118.petclinic.common.enums.VisitStatus.CANCELLED,
+                                 com.github.farzan6118.petclinic.common.enums.VisitStatus.COMPLETED)
             and v.startTime < :visitEnd
             and v.endTime > :visitStart
             and (:excludedVisitUuid is null or v.uuid <> :excludedVisitUuid)
@@ -53,8 +53,8 @@ public interface VisitRepository extends JpaRepository<Visit, Long> {
             select case when count(v) > 0 then true else false end
             from Visit v
             where v.vet.uuid = :vetUuid
-            and v.status not in (com.github.farzan6118.petclinic.model.constant.VisitStatus.CANCELLED,
-                                 com.github.farzan6118.petclinic.model.constant.VisitStatus.COMPLETED)
+            and v.status not in (com.github.farzan6118.petclinic.common.enums.VisitStatus.CANCELLED,
+                                 com.github.farzan6118.petclinic.common.enums.VisitStatus.COMPLETED)
             and v.startTime < :visitEnd
             and v.endTime > :visitStart
             and (:excludedVisitUuid is null or v.uuid <> :excludedVisitUuid)
@@ -70,8 +70,8 @@ public interface VisitRepository extends JpaRepository<Visit, Long> {
             select case when count(v) > 0 then true else false end
             from Visit v
             where v.room.uuid = :roomUuid
-            and v.status not in (com.github.farzan6118.petclinic.model.constant.VisitStatus.CANCELLED,
-                                 com.github.farzan6118.petclinic.model.constant.VisitStatus.COMPLETED)
+            and v.status not in (com.github.farzan6118.petclinic.common.enums.VisitStatus.CANCELLED,
+                                 com.github.farzan6118.petclinic.common.enums.VisitStatus.COMPLETED)
             and v.startTime < :visitEnd
             and v.endTime > :visitStart
             and (:excludedVisitUuid is null or v.uuid <> :excludedVisitUuid)
